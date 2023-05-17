4e1e0832fb7de4dde072df764ad208adc3ff4b3a
Angel <ayog95@gmail.com>
2023-05-17 12:26:19 -0600

    Ultimos cambios

--- src/main/java/com/notas/primerProyecto/configuration/WebSecurity.java
+++ src/main/java/com/notas/primerProyecto/configuration/WebSecurity.java
@@ -1,11 +1,11 @@
 package com.notas.primerProyecto.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfiguration {
+// @Configuration
+// @EnableWebSecurity
+// public class WebSecurity extends WebSecurityConfiguration {
 
-}
+// }
